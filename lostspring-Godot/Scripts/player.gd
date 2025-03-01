23f894a7eb923eaeff9a82aa3feebe86e7321655
PC-Pro <fablab@cocotte-numerique.fr>
2025-03-01 14:11:56 +0100

    Ajout du split screen

--- lostspring-Godot/Scripts/player.gd
+++ lostspring-Godot/Scripts/player.gd
@@ -25,7 +25,8 @@ func _physics_process(delta):
 
 	direction.x = Input.get_axis(arrayInput[0],arrayInput[1])
 	direction.y = Input.get_axis(arrayInput[2],arrayInput[3])
-	velocity = speed * direction
+	velocity.x = int(speed * direction.x)
+	velocity.y = int(speed * direction.y)
 	if direction == Vector2(0,0):
 		if playerSprite.animation == "runSide":
 			playerSprite.animation = "idleSide"
@@ -38,9 +39,9 @@ func _physics_process(delta):
 		playerSprite.flip_h = false
 		playerSprite.animation = "runSide"
 	elif direction.y > 0:
-		playerSprite.animation = "runUp"
-	elif direction.y < 0:
 		playerSprite.animation = "runDown"
+	elif direction.y < 0:
+		playerSprite.animation = "runUp"
 	playerSprite.play()
 	
 	move_and_slide()

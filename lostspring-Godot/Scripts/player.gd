532d4f9f48a284eff64bf00ea944dc0952fbbc19
PC-Pro <fablab@cocotte-numerique.fr>
2025-03-01 13:00:06 +0100

    Ajout des animations

--- lostspring-Godot/Scripts/player.gd
+++ lostspring-Godot/Scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 @export var speed = 200
 var arrayInput = ["left","right","top","bottom"]
+@onready var playerSprite = $AnimatedSprite2D
 
 
 func _ready():
@@ -15,19 +16,31 @@ func _ready():
 		print(num)
 		arrayInput[i] = arrayInput[i] + str(num)
 		print(arrayInput[i])
-
 func _physics_process(delta):
 	
 	var direction = Vector2(0,0)
+
+		
 	#Inputs
+
 	direction.x = Input.get_axis(arrayInput[0],arrayInput[1])
 	direction.y = Input.get_axis(arrayInput[2],arrayInput[3])
 	velocity = speed * direction
-	
+	if direction == Vector2(0,0):
+		if playerSprite.animation == "runSide":
+			playerSprite.animation = "idleSide"
+		else:
+			playerSprite.animation = "idleFace"
 	if direction.x < 0:
-		$AnimatedSprite2D.flip_h = true
-	if direction.x > 0:
-		$AnimatedSprite2D.flip_h = false
-	
+		playerSprite.flip_h = true
+		playerSprite.animation = "runSide"
+	elif direction.x > 0:
+		playerSprite.flip_h = false
+		playerSprite.animation = "runSide"
+	elif direction.y > 0:
+		playerSprite.animation = "runUp"
+	elif direction.y < 0:
+		playerSprite.animation = "runDown"
+	playerSprite.play()
 	
 	move_and_slide()

1f58141203d853a4c207ab05e032266cd3cf99f9
PC-Pro <fablab@cocotte-numerique.fr>
2025-03-02 17:35:24 +0100

    EndGameMusic ETC

--- lostspring-Godot/Scenes/ambiance.gd
+++ lostspring-Godot/Scenes/ambiance.gd
@@ -9,8 +9,10 @@ var startVolume = 1.0
 func _process(delta: float) -> void:
 	if isFading==true:
 		t += delta * 0.7
-		$"../Start Music".volume_db = lerp(1.0,0.0,t)
-		print("is fading at sound " + str($"../Start Music".volume_db))
+		#$"../Start Music".volume_db = lerp(1.0,0.0,t)
+		#var fade_in_music = create_tween()
+		#fade_in_music.tween_property($"../Start Music", "volume_db", linear_to_db(1.0), 1.0).from(linear_to_db(0.1))
+		#print("is fading at sound " + str($"../Start Music".volume_db))
 		self.volume_db = lerp(0.0,1.0,t)
 	if $"../Start Music".volume_db < 0.001 && isFading == true:
 		isFading = false
--- lostspring-Godot/Scenes/flower_ui.gd
+++ lostspring-Godot/Scenes/flower_ui.gd
@@ -1,6 +1,6 @@
 extends Node
 var endGame = [false,false]
-
+var endMusicPlay = false
 func flowerColorUI(colorName,playerNumber) -> void:
 	var uiNode = get_node("/root/Node2D/UI/FlowerUI" + str(playerNumber))
 	uiNode.modulate = Color(colorName)
@@ -11,8 +11,12 @@ func flowerColorUI(colorName,playerNumber) -> void:
 
 func endOfTheGame(playerState,playerNumber) -> void:
 	endGame[playerNumber -1] = playerState
-	if endGame == [true,true]:
-		$CreditsScreen.visible = true
+	if endGame == [true,true] && endMusicPlay == false:
+		$CreditsScreen/Timer.start()
+		$"../AudioPlayer/EndMusic".play()
+		$"../AudioPlayer/Ambiance".stop()
+		endMusicPlay = true
+		
 
 func showMap(visibility,currentPlayer) -> void:
 	var mapNode = get_node("/root/Node2D/UI/Map" + str(currentPlayer))
@@ -21,3 +25,7 @@ func showMap(visibility,currentPlayer) -> void:
 		mapNode.visible = true
 	else:
 		mapNode.visible = false
+
+
+func _on_timer_timeout() -> void:
+	$CreditsScreen.visible = true
